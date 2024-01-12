8b770fbc1295e19c62d3ab07d7f68ed46a1d560c
akatsukioshiro <ashishnair.oph@gmail.com>
2024-01-13 03:39:01 +0530

    session creation, auto server creation, dockerfile and compose gen

---
+++ flask_session_server.py
@@ -0,0 +1,85 @@
+from flask import Flask, render_template, request
+import socket, json, os
+from datetime import datetime
+
+app = Flask(__name__)
+port_in_use = {}
+
+@app.route('/')
+def index():
+    return render_template('session_creator.j2')
+
+def is_port_free(port):
+    try:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.bind(("127.0.0.1", port))
+        return "OPEN"
+    except socket.error:
+        return "IN USE / CLOSED"
+
+@app.route('/port_allocate')
+def port_alloc():
+    this_port = 0
+    for this_port in range(49152, 65536, 1):
+        if str(this_port) not in port_in_use:
+            p_stat = is_port_free(this_port)
+            if p_stat == "OPEN":
+                p_stat = "IN USE / CLOSED"
+                client_ip = request.headers.get('X-Forwarded-For')
+                if not client_ip:
+                    client_ip = request.remote_addr
+                port_in_use[str(this_port)] = { "source_ip": [client_ip], "inactivity_timer": "0 minutes" }
+                break
+    timestamp    = datetime.now().strftime("%Y_%m_%d_%H_%M_%S_%f")
+    base_path    = "/mnt/f/Desktop/test/dockerScriptRunner"
+    session_path = os.path.join(base_path, "session_dir", timestamp)
+    os.makedirs(session_path)
+    trig_cmd = "nohup {base_path}/env/bin/python run_flask_server.py --port {this_port} --session {timestamp} > {base_path}/session_dir/{timestamp}/flask_log.txt 2>&1 &".format(base_path=base_path, timestamp=timestamp, this_port=str(this_port))
+    os.system(trig_cmd)
+    return json.dumps({'success': True, 'this_port': str(this_port), 'session': timestamp}), 200, {'ContentType':'application/json'}
+
+@app.route('/port_availability')
+def port_avail():
+    # Example data
+    port_status_entries = []
+    for this_port in range(49152, 65536, 1):
+        if str(this_port) in port_in_use:
+            p_stat = "IN USE / CLOSED"
+            src_ip = ", ".join(port_in_use[str(this_port)]["source_ip"])
+            i_time = "{tm} minutes".format(tm=port_in_use[str(this_port)]["inactivity_timer"]) 
+        else:
+            p_stat = is_port_free(this_port)
+            src_ip = "-"
+            i_time = "-"
+        lit = {'port': this_port, 'status': p_stat, 'source_ip': src_ip, 'inactivity_timer': i_time}
+        port_status_entries.append(lit)
+
+    return render_template('port_avail.j2', port_status_entries=port_status_entries)
+
+@app.route('/port_heartbeat')
+def port_hb():
+    # Attempt to get the client IP from X-Forwarded-For header (common in proxy setups)
+    client_ip = request.headers.get('X-Forwarded-For')
+
+    if not client_ip:
+        # If X-Forwarded-For header is not present, use the remote_addr attribute
+        client_ip = request.remote_addr
+
+    this_port = request.args.get("this_port")
+    inactive  = request.args.get("inactive")
+    if port_in_use.get(this_port, "NA") == "NA":
+        port_in_use[this_port] = { "source_ip": [client_ip], "inactivity_timer": inactive }
+    else:
+        port_in_use[this_port]["inactivity_timer"] = inactive
+
+        if client_ip not in port_in_use[this_port]["source_ip"]:
+            port_in_use[this_port]["source_ip"].append(client_ip)
+
+    print(port_in_use)
+
+    return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=8778)
+
--- run_flask_server.py
+++ run_flask_server.py
@@ -1,14 +1,25 @@
-from flask import Flask, render_template
-import os, json
+from flask import Flask, render_template, request
+import os, json, zipfile, shutil, argparse
+#from datetime import datetime
 
 app = Flask(__name__)
+DEFAULT_PORT = 5000
+parser = argparse.ArgumentParser(description='Run a Flask application with a custom port.')
+parser.add_argument('--port', type=int, help='Port number to run the Flask app on')
+parser.add_argument('--session', type=str, help='Session Directory to store files in')
+args = parser.parse_args()
+this_port = args.port if args.port else DEFAULT_PORT
 
 @app.route('/')
 def index():
     
+    #timestamp          = datetime.now().strftime("%Y_%m_%d_%H_%M_%S_%f")
+    session_path       = os.path.join("/mnt/f/Desktop/test/dockerScriptRunner/session_dir", args.session)
+    #os.makedirs(session_path)
+
     logical_proc_count = os.cpu_count()
-    mem_bytes = os.sysconf('SC_PAGE_SIZE') * os.sysconf('SC_PHYS_PAGES')
-    mem_gib = int(mem_bytes/(1024.**3))
+    mem_bytes          = os.sysconf('SC_PAGE_SIZE') * os.sysconf('SC_PHYS_PAGES')
+    mem_gib            = int(mem_bytes/(1024.**3))
 
     inp_ops = {}
     sys_ops = {
@@ -17,7 +28,49 @@ def index():
             }
     with open(os.path.join("data", "input_ops.json")) as f:
         inp_ops = json.loads(f.read())
-    return render_template('base.j2', inp_ops=inp_ops, sys_ops=sys_ops)
+    return render_template('base.j2', inp_ops=inp_ops, sys_ops=sys_ops, session_path=session_path, this_port=this_port)
+
+@app.route('/process_code_zip', methods = ["GET", "POST"])
+def process_code():
+
+    if request.method == 'POST':
+        inp_info          = json.loads(request.form.get("inp_info"))
+        zip_file          = request.files.get("zip_file")
+        path_to_zip_file  = os.path.join(inp_info.get("session_p"), zip_file.filename)
+        zip_dir_name      = zip_file.filename.split(".zip")[0].strip()
+        dir_to_extract_to = os.path.join(inp_info.get("session_p"), zip_dir_name)
+        session_name      = os.path.basename(inp_info.get("session_p"))
+        dockerf_template  = render_template("Dockerfile.j2", inp_info=inp_info, session_name=session_name)
+        dockerc_template  = render_template("docker-compose.j2", session_name=session_name, session_dir=inp_info.get("session_p"))
+        usr_request_json  = json.loads(inp_info.get("inp_tarea"))
+        run_script_templ  = render_template("runScript.j2", usr_request_json=usr_request_json)
+
+        shutil.rmtree(inp_info.get("session_p"))
+        shutil.os.makedirs(inp_info.get("session_p"))
+
+        with open(path_to_zip_file, "wb+") as f:
+            f.write(zip_file.read())
+
+        with zipfile.ZipFile(path_to_zip_file, 'r') as zip_ref:
+            zip_ref.extractall(inp_info.get("session_p"))
+
+        os.remove(path_to_zip_file)
+        for item in os.listdir(dir_to_extract_to):
+            item_path = os.path.join(dir_to_extract_to, item)
+            shutil.move(item_path, inp_info.get("session_p"))
+
+        os.rmdir(dir_to_extract_to)
+
+        with open(os.path.join(inp_info.get("session_p"), "Dockerfile"), "w+") as f:
+            f.write(dockerf_template)
+
+        with open(os.path.join(inp_info.get("session_p"), "docker-compose.yml"), "w+") as f:
+            f.write(dockerc_template)
+
+        with open(os.path.join(inp_info.get("session_p"), "runScript.sh"), "w+") as f:
+            f.write(run_script_templ)
+
+    return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=8668)
+    app.run(debug=True, host="0.0.0.0", port=this_port)
